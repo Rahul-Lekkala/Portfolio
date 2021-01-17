77b27ef0802df248be02e76675d6638606bae641
Rahul Lekkala <rahul.lekkala97@gmail.com>
2021-01-17 09:54:35 +0530

    Initial Commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Rahul Lekkala",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Rahul and I'm a Programmer who likes to develop websites!",
+  githubUrl: "https://github.com/Rahul-Lekkala",
+  linkedinUrl: "https://www.linkedin.com/in/rahul-lekkala-741592138/",
+  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+};
